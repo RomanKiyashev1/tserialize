6f635cfdf19b801dfd2afd57df6971c0b05b8307
Roman <roman_kiyashev@outlook.com>
2024-06-25 00:24:21 +0300

    fix formatting

--- src/serialize/serialize.ts
+++ src/serialize/serialize.ts
@@ -2,33 +2,33 @@ import { TSerializeConfig } from './../core/types';
 import { ClassMetaStore, ParentKey, PropertyMetadata, RootMetaStore } from './../core';
 
 function serializeValue(metadata: PropertyMetadata, value: any, instance: any, config: TSerializeConfig): any {
-  if (!metadata) {
-    return;
-  }
-
-  if (metadata.isStruct) {
-    const serializer = metadata.serialize;
-    if (serializer) {
-      return serializer(value, instance, config)
+    if (!metadata) {
+        return;
     }
 
-    return value ? serialize(value, config) : null;
-  } else {
-    const serializer = metadata.serialize;
-    return serializer ? serializer(value, instance, config) : value;
-  }
+    if (metadata.isStruct) {
+        const serializer = metadata.serialize;
+        if (serializer) {
+            return serializer(value, instance, config)
+        }
+
+        return value ? serialize(value, config) : null;
+    } else {
+        const serializer = metadata.serialize;
+        return serializer ? serializer(value, instance, config) : value;
+    }
 }
 
 function assignSerializedValueToResult(metadata: PropertyMetadata, serializedValue: any, result: any, config: TSerializeConfig): void {
-  const nonSerializableValues = config.allowNullValues ? [undefined] : [undefined, null];
-  if (!nonSerializableValues.includes(serializedValue)) {
-    const jsonName = metadata.rawKey;
-    if (jsonName !== ParentKey) {
-      result[jsonName] = serializedValue;
-    } else {
-      Object.assign(result, serializedValue);
+    const nonSerializableValues = config.allowNullValues ? [undefined] : [undefined, null];
+    if (!nonSerializableValues.includes(serializedValue)) {
+        const jsonName = metadata.rawKey;
+        if (jsonName !== ParentKey) {
+            result[jsonName] = serializedValue;
+        } else {
+            Object.assign(result, serializedValue);
+        }
     }
-  }
 }
 
 /**
@@ -38,30 +38,30 @@ function assignSerializedValueToResult(metadata: PropertyMetadata, serializedVal
  * @returns {{}} - обычный объект JS
  */
 export function serialize(model: { [key: string]: any }, config: TSerializeConfig = { allowNullValues: false, autoCreateModelForRawData: false }): object {
-  const result = {};
-  const targetClass = Object.getPrototypeOf(model);
+    const result = {};
+    const targetClass = Object.getPrototypeOf(model);
 
-  let metaStore: ClassMetaStore = RootMetaStore.getClassMetaStore(targetClass);
-  // Всякое бывает, мб кто-то сериализует объект без декораторов
-  if (!metaStore) {
-    const possibleParent = Object.getPrototypeOf(targetClass);
+    let metaStore: ClassMetaStore = RootMetaStore.getClassMetaStore(targetClass);
+    // Всякое бывает, мб кто-то сериализует объект без декораторов
+    if (!metaStore) {
+        const possibleParent = Object.getPrototypeOf(targetClass);
 
-    if (possibleParent && possibleParent.constructor.name !== 'Object') {
-      // TODO make while loop until reach Object prototype or exists metaStore
-      metaStore = RootMetaStore.getClassMetaStore(possibleParent);
-      if (!metaStore) {
-        return {};
-      }
-    } else {
-      return {};
+        if (possibleParent && possibleParent.constructor.name !== 'Object') {
+            // TODO make while loop until reach Object prototype or exists metaStore
+            metaStore = RootMetaStore.getClassMetaStore(possibleParent);
+            if (!metaStore) {
+                return {};
+            }
+        } else {
+            return {};
+        }
     }
-  }
 
-  const modelKeys = metaStore.propertyKeys;
-  for (const propertyKey of modelKeys) {
-    const metadata = metaStore.getMetadataByPropertyKey(propertyKey);
-    const serializedValue = serializeValue(metadata, model[propertyKey], model, config);
-    assignSerializedValueToResult(metadata, serializedValue, result, config);
-  }
-  return result;
+    const modelKeys = metaStore.propertyKeys;
+    for (const propertyKey of modelKeys) {
+        const metadata = metaStore.getMetadataByPropertyKey(propertyKey);
+        const serializedValue = serializeValue(metadata, model[propertyKey], model, config);
+        assignSerializedValueToResult(metadata, serializedValue, result, config);
+    }
+    return result;
 }
